eec22f0b30e0d2261d9c31ef4c9b957136189e34
Eman talal <105923938+EmanTalal@users.noreply.github.com>
2022-10-03 12:59:05 +0300

    Add files via upload

---
+++ Kotlin.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication
+
+    fun main(){
+        var number1=10;
+        var number2=2;
+        println("The number2 Greater than number2  :  ${number1 == number2 && number1 <number2}")
+
+        println("------------------------------")
+        var num1=10;
+        var num2=3;
+        println("the result mod is : ${num1%num2}")
+
+        println("------------------------------")
+
+        var numb1=5;
+        var numb2=5;
+        println(" The numb1 Greater than or equal !: ${numb1>=numb2}")
+
+        println("------------------------------")
+
+        var aegEman=20;
+        var agrmaha =30;
+
+        println(" age Eman  Greater than  Maha or agr Eman  equal != ageMaha  :  ${aegEman>agrmaha ||aegEman!=agrmaha }")
+
+        println("------------------------------")
+
+        var n1=3;
+        var n2=2;
+        var n8=5;
+        println("the result  is : ${(n1*n2)-n8}")
+
+        println("------------------------------")
+
+        var n3=2
+        var n4=3;
+        println(" the n3 equal n4 ! : ${n4!=n3}")
+
+        println("------------------------------")
+        var n5=6;
+        var n6=6;
+        var n7=3;
+        println("The resule is : ${(n5+n6)/n7}")
+
+
+
+
+
+
+
+
+
+    }
